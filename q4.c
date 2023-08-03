94318ca11a087a6b5d92e16f569e436c94781bc3
rafael-pf <132072483+rafael-pf@users.noreply.github.com>
2023-08-03 09:41:45 -0300

    Update q4.c

--- q4.c
+++ q4.c
@@ -5,28 +5,44 @@
 int main(){
     int tam=0, i=0;
     char string[100];
-    char **p;
-    char **pAux;
+    char **p=NULL;
+    char **pAux=NULL;
 
-    printf("Digite as palavras: \n");
+    printf("->Digite as palavras: \n");
     while(scanf(" %s", string)!=EOF){
         pAux = p;
+        //realocar vetor dinamico de string a cada entrada
         p = (char **) realloc(pAux, (tam+1)* sizeof(char *));
         if(p==NULL){
             printf("Problema de alocacao!\n");
+            for(i=0; i<tam; i++){
+                free(p[i]);
+            }
             free(p);
             exit(1);
         }
+        //alocar o espaço para a string
         p[tam] = (char *) malloc((strlen(string)+1) * sizeof(char));
+        if(p[tam]==NULL){
+            printf("Problema de alocacao!\n");
+            for(i=0; i<tam; i++){
+                free(p[i]);
+            }
+            free(p);
+            exit(1);
+        }
+        //colocar a string no vetor e aumentar o tamanho
         strcpy(p[tam++], string);
     }
 
-    printf("Palavras armazenadas: \n");
+    printf("->Palavras armazenadas: \n");
 
+    //printar palavras
     for(i=0; i<tam; i++){
         printf("\t-%s\n", p[i]);
     }
 
+    //free
     for(i=0; i<tam; i++){
         free(p[i]);
     }
