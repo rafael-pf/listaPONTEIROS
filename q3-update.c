fc298be9a25478cac689948903f1ba65e5f6555e
rafael-pf <132072483+rafael-pf@users.noreply.github.com>
2023-08-03 09:53:32 -0300

    Update q3-update.c

--- q3-update.c
+++ q3-update.c
@@ -8,18 +8,24 @@ int main(){
     int i=-1;
     scanf(" %s", str);
     
+    //alterar o endereco de memoria anterior a string para \0 (necessario para o ultimo loop)
     *(ptr_str-1)='\0';
 
+    //zerar a str_inv
     for(i=0; i<50; i++){
         *(ptr_inv+i)='\0';
     }
 
     i=0;
+
+    //procurar ultimo caractere e fazer ptr_str apontar para ele
     do{
         ptr_str=&*(ptr_str+1);
     }while(*(ptr_str+1)!='\0');
 
     i=0;
+
+    //copiar caracteres de traz pra frente de *ptr_str para *ptr_inv (até achar o \0 que eu declarei no inicio)    
     do{
         *(ptr_inv+i)=*(ptr_str-i);
         i++;
